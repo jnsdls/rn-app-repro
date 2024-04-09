c43b3f9ffcc2efcc59c054642521c39e818765c4
Jonas Daniels <jonas.daniels@outlook.com>
2024-04-09 09:51:03 -0700

    init repro

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
+// Import polyfills
+import "@thirdweb-dev/react-native-compat";
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { ThirdwebProvider, useEmbeddedWallet, embeddedWallet, useAddress } from "@thirdweb-dev/react-native";
+
+ 
+const embeddedWalletConfig = embeddedWallet();
 
 export default function App() {
   return (
+    <ThirdwebProvider clientId="697e11dc360f42399d8c1e67384bfcc1" supportedWallets={[
+      embeddedWalletConfig
+    ]}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Component />
+      <Address />
       <StatusBar style="auto" />
     </View>
+    </ThirdwebProvider>
   );
+}
+
+function Component(){
+  const embeddedWallet = useEmbeddedWallet();
+  const onPress = async () => {
+    embeddedWallet.connect({
+      strategy: "auth_endpoint",
+      payload: "277a696f-ba5a-4924-8a6a-5c2cf06bddb4",
+      encryptionKey: "testing",
+    });
+  };
+
+  return (<Button onPress={onPress} title="Custom Auth" />);
+}
+
+function Address(){
+  const address = useAddress();
+  return <Text>{address}</Text>
 }
 
 const styles = StyleSheet.create({
---
+++ metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const extraNodeModules = require("node-libs-browser");
+ 
+const config = getDefaultConfig(__dirname);
+ 
+config.resolver.extraNodeModules = extraNodeModules;
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+ 
+module.exports = config;
